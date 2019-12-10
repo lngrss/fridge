fd74c088f50eb1a1d6ee52ae9a0a0dcca51c9d25
Magdalena Großmann <magdalena.grossmann@solarisbank.de>
2019-12-10 10:04:51 +0100

    Fixes nickname method

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,11 +1,11 @@
 class Ingredient < ApplicationRecord
-  def nick_name()
-    if color & brand
-      color[0..2] + brand[-3..]
-    elsif color
-      color[0..2] + "ish"
-    elsif brand
-      "Me" + brand[-3..]
+  def nick_name
+    if colour & name
+      colour[0..2] + name[-3..]
+    elsif colour
+      colour[0..2] + "ish"
+    elsif name
+      "Me" + name[-3..]
     else
       "IHaveALazyOwner"
     end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,8 +1,11 @@
+#require_relative '../app/models/ingredient.rb'
+require "pry"
+
 describe "#nick_name" do
   subject { ingredient.nick_name() }
-  context "when the ingredient has a colour and brand" do
-    let(:ingredient) { binding.pry ; Ingredient.create({ :brand => "Teseract", :colour => "orange"}) }
-    it "returns the first three letters of colour and last three of brand as nickname" do
+  context "when the ingredient has a colour and name" do
+    let( :ingredient) { binding.pry; Ingredient.create({ :name => "Teseract", :colour => "orange"}) }
+    it "returns the first three letters of colour and last three of name as nickname" do
       expect subject.to eq "oraact"
     end
   end
@@ -11,6 +14,20 @@ describe "#nick_name" do
   #   let(:ingredient) { Ingredient.create({ :brand => "Teseract", :colour => "orange"}) }
   #   it "returns the first three letters of colour and ish" do
   #     expect { subject } to eq "oraish"
+  #   end
+  # end
+
+  # context "when the ingredient has no colour and a brand" do
+  #   let(:ingredient) { Ingredient.create({ :brand => "Teseract", :colour => "orange"}) }
+  #   it "returns Me and the last three letters of colour" do
+  #     expect { subject } to eq "Meact"
+  #   end
+  # end
+
+  # context "when the ingredient has no colour and no brand" do
+  #   let(:ingredient) { Ingredient.create({ :brand => "Teseract", :colour => "orange"}) }
+  #   it "returns string IHaveALazyOwner" do
+  #     expect { subject } to eq "IHaveALazyOwner"
   #   end
   # end
 end
